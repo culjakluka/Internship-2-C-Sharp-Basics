f5d93467298d5cf38fee7f63400cde484282f392
culjakluka <68790066+culjakluka@users.noreply.github.com>
2020-11-23 14:02:58 +0100

    case 4 done, case 5 in progress

--- CSharp_Basics/CSharp_Basics/Program.cs
+++ CSharp_Basics/CSharp_Basics/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace CSharp_Basics
@@ -8,44 +8,50 @@ namespace CSharp_Basics
         static void Main(string[] args)
         {
             var dict = new Dictionary<int, string>();
-            var akcija = 1;
-            Console.WriteLine("Odaberite akciju:\n1 - Ispis cijele liste\n2 - Ispis imena pjesme unosom pripadajućeg rednog broja\n3 - Ispis rednog broja pjesme unosom pripadajućeg imena\n4 - Unos nove pjesme\n5 - Brisanje pjesme po rednom broju\n6 - Brisanje pjesme po imenu\n7 - Brisanje cijele liste\n8 - Uređivanje imena pjesme\n9 - Uređivanje rednog broja pjesme, odnosno premještanje pjesme na novi redni broj u listi\n0 - Izlaz iz aplikacije");
-
-            while (akcija != 0) 
+            var action = 1;
+           
+            while (action != 0) 
             {
-                akcija = int.Parse(Console.ReadLine());
-                switch (akcija)
+                Console.WriteLine("Odaberite akciju:\n-1 - Ispis cijele liste\n2 - Ispis imena pjesme unosom pripadajućeg rednog broja\n3 - Ispis rednog broja pjesme unosom pripadajućeg imena\n4 - Unos nove pjesme\n5 - Brisanje pjesme po rednom broju\n6 - Brisanje pjesme po imenu\n7 - Brisanje cijele liste\n8 - Uređivanje imena pjesme\n9 - Uređivanje rednog broja pjesme, odnosno premještanje pjesme na novi redni broj u listi\n0 - Izlaz iz aplikacije");
+                action = int.Parse(Console.ReadLine());
+                switch (action)
                 {
                     case 1:
-                        ispisListe(dict);
+                        IspisListe(dict);
                         break;
                     case 2:
-                        ispisPoBroju(dict);
+                        IspisPoBroju(dict);
                         break;
                     case 3:
-                        ispisPoImenu(dict);
+                        IspisPoImenu(dict);
+                        break;
+                    case 4:
+                        UnosPjesme(dict);
+                        break;
+                    case 5:
+                        BrisanjePjesmePoRednomBroju(dict);
                         break;
                     default:
                         break;
                 }
             }
         }
-        static void ispisListe(Dictionary<int, string> dict) 
+        static void IspisListe(Dictionary<int, string> dict) 
         {
             foreach (var song in dict)
             {
                 Console.WriteLine(song.Key + ". " + song.Value);
             }
         }
-        static void ispisPoBroju(Dictionary<int, string> dict)
+        static void IspisPoBroju(Dictionary<int, string> dict)
         {
             Console.WriteLine("Unesite redni broj pjesme za koju zelite saznati ime: ");
-            var broj = int.Parse(Console.ReadLine());
+            var songNumber = int.Parse(Console.ReadLine());
             var succ = 0;
 
             foreach (var song in dict)
             {
-                if (song.Key == broj) 
+                if (song.Key == songNumber) 
                 { 
                     Console.WriteLine("Trazena pjesma se zove: " + song.Value);
                     succ = 1;
@@ -53,21 +59,56 @@ namespace CSharp_Basics
             }
             if (0 == succ) { Console.WriteLine("Pjesma nije nadena!"); }
         }
-        static void ispisPoImenu(Dictionary<int, string> dict)
+        static void IspisPoImenu(Dictionary<int, string> dict)
         {
             Console.WriteLine("Unesite ime pjesme za koji zelite saznati redni broj: ");
-            var ime = Console.ReadLine();
+            var songName = Console.ReadLine();
             var succ = 0;
 
             foreach(var song in dict)
             {
-                if (song.Value == ime) 
+                if (song.Value == songName) 
                 {
                     Console.WriteLine("Trazena pjesma je na broju: " + song.Key);
                     succ = 1;
                 }
             }
             if (0 == succ) { Console.WriteLine("Pjesma nije nadena!"); }
+        }
+        static void UnosPjesme(Dictionary<int, string> dict)
+        {
+            Console.WriteLine("Unesite ime pjesme: ");
+            var songName = Console.ReadLine();            
+            var index = 1;
+            foreach (var song in dict)
+            {
+                index++;
+            }
+            Console.WriteLine("Jeste li sigurni da zelite dodati pjesmu " + "'" + songName + "' u listu? (da/ne)");
+            if (Console.ReadLine() == "da")
+            {
+                dict.Add(index, songName);
+            }
+        }
+        static void BrisanjePjesmePoRednomBroju(Dictionary<int, string> dict) //TU SI BURAZ
+        {   
+            Console.WriteLine("Pjesmu po kojem rednom broju zelite izbrisati?");
+            var index = int.Parse(Console.ReadLine());
+            var tempKey = 0;
+            var tempName = "";
+            var tempDict = new Dictionary<int, string>();
+            Console.WriteLine("Jeste li sigurni da zelite ukloniti pjesmu na broju " + index + "? (da/ne)");
+            if (Console.ReadLine() == "da")
+            {
+                if (dict.Remove(index))
+                {
+                    Console.WriteLine("Pjesma uspjesno izbrisana.");
+                }
+            }
+            foreach (var song in dict)
+            {
+             
+            }
         }
     }
 }
